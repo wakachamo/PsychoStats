bb7d07eb07f0d930695860b7df8a0cd947231d48
Daniel <d4f@Daniel-Laptop-Ubuntu.(none)>
2010-05-26 23:21:37 +0200

    Fixed PHP "Deprecated" messages in the Frontend installer

--- upload/install/common.php
+++ upload/install/common.php
@@ -29,7 +29,7 @@ define("PS_INSTALLDIR", dirname(__FILE__));
 
 // enable some sane error reporting (ignore notice errors) and turn off the magic. 
 // we also want to to disable E_STRICT.
-error_reporting(E_ALL ^ E_NOTICE); 
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED); 
 set_magic_quotes_runtime(0);
 /**/
 ini_set('display_errors', 'On');
